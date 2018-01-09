9646f551d3f36d1d76e4c3798e87a8f9b13c15e7
laixiaolong <lxl1542873238@hotmail.com>
2018-01-09 22:07:04 +0800

    First Commit!

---
+++ app/src/main/java/com/example/laixiaolong/pichandler/PorterBuffActivity.java
@@ -0,0 +1,16 @@
+package com.example.laixiaolong.pichandler;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PorterBuffActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_porter_buff);
+
+    }
+}
